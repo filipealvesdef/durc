cf6e4c5185a3e45342f3f098f4cc59d1cc0ba019
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-28 10:58:38 -0300

    add utils

---
+++ vesla_pymvc/utils.py
@@ -0,0 +1,12 @@
+from copy import deepcopy
+
+
+def update_dict_fields(fields, **fields_mapping):
+    updated_fields = dict(fields)
+    for k, v in fields.items():
+        if k in fields_mapping:
+            updated_fields[k] = fields_mapping[k]
+    return updated_fields
+
+
+def clone(d): return deepcopy(d)
