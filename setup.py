7f0a13830af828acb8510233fc52989b0be8e6fe
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-28 10:57:29 -0300

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vesla_pymvc',
+    description='Some MVC utils for python projects',
+    version='1.0.0',
+    author='Filipe Alves',
+    author_email='filipe.alvesdefernando@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Cerberus',
+    ],
+    url='https://github.com/filipealves/vesla_pymvc',
+    zip_safe=False,
+)
