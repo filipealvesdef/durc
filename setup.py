b4a612d3893a8eaa62fdc90ddde1eb6a3ff389ec
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-08-28 12:00:38 -0300

    setup: update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='vesla_pymvc',
     description='Some MVC utils for python projects',
-    version='1.0.0',
+    version='1.0.1',
     author='Filipe Alves',
     author_email='filipe.alvesdefernando@gmail.com',
     packages=find_packages(),
