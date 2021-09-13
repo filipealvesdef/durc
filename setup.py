78cc6bb37f6ebe05bbd60811020eb2f78111b714
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-13 07:43:23 -0300

    setup: update version 3.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='durc',
     description='Hierarchical CRUD utils for python projects',
-    version='2.2.0',
+    version='3.0.0',
     author='Filipe Alves',
     author_email='filipe.alvesdefernando@gmail.com',
     packages=find_packages(),
